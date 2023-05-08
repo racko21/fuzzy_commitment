c941928a67581657bcbae4092531eeab97d5c064
racko21 <florianrack@gmx.at>
2023-05-08 13:05:19 +0200

    key/witness generator

--- src/key_gen.java
+++ src/key_gen.java
@@ -1,3 +1,15 @@
-public class key_gen {
-    
+
+public class key_gen {       
+    public String BitString (int length) {
+        StringBuilder keystring = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            double a = Math.random();
+            if (a <= 0.5) {
+                keystring.append("0");
+            } else {
+                keystring.append("1");
+            }
+        }
+        return keystring.toString();
+    }
 }
